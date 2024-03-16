1ed11917277465b028e1f2e5dc366ef45bffa564
Johan Böhlin <johboh@gmail.com>
2024-03-16 08:56:55 +0100

    Add integration for arduino

---
+++ integration/arduino/Integration.ino
@@ -0,0 +1,56 @@
+
+#include <Arduino.h>
+#include <EspNowCrypt.h>
+#include <EspNowHost.h>
+#include <EspNowNode.h>
+#include <EspNowPreferences.h>
+#include <driver/gpio.h>
+#include <esp_crt_bundle.h>
+
+/**
+ * This is only a verification build of the full library. Not representable for a node or a host. For that, check
+ * examples instead.
+ */
+
+#define FIRMWARE_VERSION 1
+
+const char esp_now_encryption_key[] = "0123456789ABCDEF"; // Must be exact 16 bytes long. \0 does not count.
+const char esp_now_encryption_secret[] = "01234567";      // Must be exact 8 bytes long. \0 does not count.
+
+EspNowHost::OnNewMessage _on_new_message = []() {};
+
+EspNowHost::OnApplicationMessage _on_application_message = [](EspNowHost::MessageMetadata metadata,
+                                                              const uint8_t *message) {};
+
+EspNowHost::FirmwareUpdateAvailable _firmware_update_available = [](uint64_t mac_address, uint32_t firmware_version) {
+  return std::nullopt;
+};
+
+EspNowHost::OnLog _on_host_log = [](const std::string message, const esp_log_level_t log_level) {
+  Serial.println(("EspNowHost (" + level + "): " + message).c_str());
+};
+
+EspNowNode::OnLog _on_node_log = [](const std::string message, const esp_log_level_t log_level) {
+  Serial.println(("EspNowNode (" + level + "): " + message).c_str());
+};
+
+EspNowNode::OnStatus _on_status = [](EspNowNode::Status status) {};
+
+EspNowPreferences _esp_now_preferences;
+EspNowCrypt _esp_now_crypt(esp_now_encryption_key, esp_now_encryption_secret);
+EspNowNode _esp_now_node(_esp_now_crypt, _esp_now_preferences, FIRMWARE_VERSION, _on_status, _on_host_log,
+                         arduino_esp_crt_bundle_attach);
+EspNowHost _esp_now_host(_esp_now_crypt, EspNowHost::WiFiInterface::STA, _on_new_message, _on_application_message,
+                         _firmware_update_available, _on_node_log);
+
+void setup() {
+  Serial.begin(115200);
+
+  esp_wifi_set_ps(WIFI_PS_NONE); // No sleep on WiFi to be able to receive ESP-NOW packages without being in AP mode.
+
+  _esp_now_host.setup();
+  _esp_now_preferences.initalizeNVS();
+  _esp_now_node.setup();
+}
+
+void loop() {}
